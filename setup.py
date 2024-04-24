3d0ae9a46e2ee3e0897a08d451c500aedfa8047c
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-04-24 08:35:28 -0600

    version bumps

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="nullscream",
-    version="0.1.0",
+    version="0.1.1",
     author="Capsize LLC",
     description="",
     long_description=open("README.md", "r", encoding="utf-8").read(),
